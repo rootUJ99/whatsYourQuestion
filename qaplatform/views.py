9c916a590390f297949e8a61f520b263357c43cd
Ujwal <ujwal@symboinsurance.com>
2020-04-03 02:50:10 +0530

    initial commit

--- qaplatform/views.py
+++ qaplatform/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import Question, Answer
+from .form import QuestionForm, AnswerForm
+import pdb
 # Create your views here.
 
 def index(req):
 
   context = {
-    'qList' : Question.objects.values(),
-    'aList' : Answer.objects.values()
+    'question_form' : QuestionForm(req.POST),
+    'answer_form' : AnswerForm(req.POST),
   }
-  print('context', context);
-  return render(req, 'qaplatform/index.html', context )
+  if req.method == 'POST':
+    # form = QuestionForm(req.POST)
+    if context['question_form'].is_valid():
+      print(context['question_form'])
+  else:
+    form = QuestionForm()
+  return render(req, 'qaplatform/index.html', { 'form': context})
