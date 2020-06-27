833842c5c3f680f1a7f64ce5ae26c5566815b9b0
Ujwal <ujwal99arak@gmail.com>
2020-06-27 23:27:15 +0530

    testing template with list of element

--- qaplatform/urls.py
+++ qaplatform/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ask_question, name='home'),
+    path('home/', views.list_of_questions, name='home0'),
 ]
--- qaplatform/views.py
+++ qaplatform/views.py
@@ -4,27 +4,24 @@ from django.utils import timezone
 from .models import Question, Answer
 from .form import QuestionForm, AnswerForm
 from django.views.decorators.csrf import csrf_protect
-import pdb
 # Create your views here.
 
 @csrf_protect
 def ask_question(request):
   if request.method == 'POST':
-    # create a form instance and populate it with data from the request:
     form = QuestionForm(request.POST)
-    # check whether it's valid:
     if form.is_valid():
-        # process the data in form.cleaned_data as required
-        # ...
-        # redirect to a new URL:
         question = form.cleaned_data['question']
         question_date = timezone.now()
         print(question)
         q = Question(question=question, question_date=question_date)
         q.save()
-      # return 
-
-    # if a GET (or any other method) we'll create a blank form
   else:
     form = QuestionForm()
-  return render(request, 'qaplatform/form.html', { 'form': form})
+  return render(request, 'qaplatform/form.html', {'form': form})
+
+def list_of_questions(request):
+  qlist = list(Question.objects.all())
+  print(qlist)
+
+  return render(request, 'qaplatform/list.html', {'qlist': qlist})
