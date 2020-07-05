41d39c7065e4f3739b3d2706823a2707dfe8001e
Ujwal <ujwal99arak@gmail.com>
2020-07-05 23:47:55 +0530

    added login and registeration form

--- whatsYourQuestion/urls.py
+++ whatsYourQuestion/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('qaplatform.urls')),
+    path('uaa/', include('auth.urls')),
     path('admin/', admin.site.urls),
 ]
