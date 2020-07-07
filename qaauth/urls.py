f91bd367a600d579176d91ead04e804d83155e6f
Ujwal <ujwal99arak@gmail.com>
2020-07-07 13:15:46 +0530

    added login flow logic

--- qaauth/urls.py
+++ qaauth/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 app_name = 'qaauth'
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.sign_in, name='login'),
     path('register/', views.register, name='register')
 ]
--- qaauth/views.py
+++ qaauth/views.py
@@ -1,24 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 # Create your views here.
-def login(request):
+def sign_in(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(request=request, data=request.POST)
+        print(form.is_valid())
         if form.is_valid():
             print(form.cleaned_data)
-            user = authenticate(username='john', password='secret')
+            username, password = form.cleaned_data
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+            return redirect('/')
+        return redirect('auth:register')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
 def register(request):
-    form = UserCreationForm()
+    form = UserCreationForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
-            user = User.objects.create_user()
-            user.save()
+            form.save()
+            print(form.cleaned_data)
+            return redirect('/')
+    else:
+        form = UserCreationForm()  
         
     return render(request, 'register.html', {'form': form})
