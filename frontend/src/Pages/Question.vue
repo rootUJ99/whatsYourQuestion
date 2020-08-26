965b5c6dc2173188aa9494544cbc8d2bc9ccc56d
Ujwal <ujwal99arak@gmail.com>
2020-08-26 23:54:51 +0530

    added seperator in comment section

--- frontend/src/Pages/Question.vue
+++ frontend/src/Pages/Question.vue
@@ -9,7 +9,9 @@
               <p>{{a.answer}}</p>
               <div v-if="a.comment.length">
                 <div v-for="c in a.comment" :key="c.id">
-                  &nbsp; &nbsp;{{c.comment}}
+                  <div class="comment_container">
+                    <div class="veritcal_hr"/>
+                  {{c.comment}}
                 </div>
               </div>
            <textarea class="input_answer" name="comment" v-model="comment"/>
@@ -23,6 +25,15 @@
     </div>
   </div>
 </template>
+<style scoped>
+.comment_container {
+  padding: 0.7rem;
+  margin: 0.5rem;
+  border-left: 0.1rem solid gray;
+}
+.veritcal_hr {
+}
+</style>
 <script>
 import axios from 'axios';
 export default {
