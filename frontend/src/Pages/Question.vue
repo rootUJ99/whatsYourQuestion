7ee284e66893b0185d1c92e5966f49aa8403cf67
Ujwal <ujwal99arak@gmail.com>
2020-08-27 23:50:32 +0530

    added working version of comment

--- frontend/src/Pages/Question.vue
+++ frontend/src/Pages/Question.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="hcenter">
     <div class="flex_column">
-        <div class="card_list">
-                {{list?.question?.question}}
-        </div>
-        <div v-if="list?.answers?.length" >
-          <div v-for="a in list?.answers" class="card_list" :key="a.id">
-              <p>{{a.answer}}</p>
-              <div v-if="a.comment.length">
-                <div v-for="c in a.comment" :key="c.id">
-                  <div class="comment_container">
-                    <div class="veritcal_hr"/>
-                  {{c.comment}}
-                </div>
+      <div class="card_list">
+        {{ list?.question?.question }}
+      </div>
+      <div v-if="list?.answers?.length">
+        <div v-for="(a,index) in list?.answers" class="card_list" :key="a.id">
+          <p>{{ a.answer }}</p>
+          <div v-if="a.comment.length">
+            <div v-for="c in a.comment" :key="c.id">
+              <div class="comment_container">
+                <div class="veritcal_hr" />
+                {{ c.comment }}
               </div>
-           <textarea class="input_answer" name="comment" v-model="comment"/>
-           <button class="answer_button" @click="onSubmitComment(a.id)">Comment</button>
+            </div>
           </div>
+          <textarea class="input_answer" name="comment" v-model="comment[index]" />
+          <button class="answer_button" @click="onSubmitComment(a.id, index)">
+            Comment
+          </button>
         </div>
-    <div class="card_list">
-        <textarea class="input_answer" name="answer" v-model="answer"/>
+      </div>
+      <div class="card_list">
+        <textarea class="input_answer" name="answer" v-model="answer" />
         <button class="answer_button" @click="onSubmit">Answer</button>
-    </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,52 +38,56 @@
 }
 </style>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  name: 'Question',
+  name: "Question",
   data() {
     return {
       list: null,
       answer: null,
-      comment: null,
-      }
+      comment: [],
+    };
   },
   async mounted() {
     try {
-        const res = await axios.get(`http://localhost:8000/api/question-answer-list/${this.$route.params.id}/`);
-        console.log(res.data)
-        this.list = res?.data
-      } catch (err) {
-        console.log('err', err)
-      }
+      const res = await axios.get(
+        `http://localhost:8000/api/question-answer-list/${this.$route.params.id}/`
+      );
+      console.log(res.data);
+      this.list = res?.data;
+    } catch (err) {
+      console.log("err", err);
+    }
   },
   methods: {
-    async onSubmit(){
+    async onSubmit() {
       try {
-        const res = await axios.post('http://localhost:8000/api/post-answer', {
+        const res = await axios.post("http://localhost:8000/api/post-answer", {
           answer: this.answer,
           question_id: this.list.question.id,
-          user_id:  this.list.question.user_id,
+          user_id: this.list.question.user_id,
         });
-        this.list = res?.data
-        this.answer = null
-      } catch(err) {
-        console.log('err', err);
+        this.list = res?.data;
+        this.answer = null;
+      } catch (err) {
+        console.log("err", err);
       }
       // this.answer=null;
     },
-    async onSubmitComment(answer_id) {
+    async onSubmitComment(answer_id, index) {
       try {
-        const res = await axios.post('http://localhost:8000/api/post-comment', {
-          comment: this.comment,
+        const res = await axios.post("http://localhost:8000/api/post-comment", {
+          comment: this.comment[index],
           answer_id,
           user_id: this.list.question.user_id,
           question_id: this.list.question.id,
-        })
-      } catch(err) {
-        console.log('err', err);
+        });
+        this.list = res?.data;
+        delete this.comment[index]
+      } catch (err) {
+        console.log("err", err);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
