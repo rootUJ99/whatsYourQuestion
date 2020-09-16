096924f865044b7e3c7f8ea90242b6487856cc48
Ujwal <ujwal99arak@gmail.com>
2020-09-17 03:30:37 +0530

    added profile model

--- qaauth/models.py
+++ qaauth/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    followers = models.IntegerField(default=0)
+    following = models.IntegerField(default=0)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user= instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+        instance.Profile.save()
+
+    
