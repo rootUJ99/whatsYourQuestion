7a8535439d3aaaccb3471fd4ba624c342b42dc2c
Ujwal <ujwal99arak@gmail.com>
2020-10-14 22:55:42 +0530

    added new migrations to remove and rename fields

--- qaplatform/models.py
+++ qaplatform/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-class Vote(models.Model):
-	upvote = models.ForeignKey(get_user_model(), related_name='upvoter', null=True, blank=True, default=None, on_delete=models.CASCADE
-    )
-	downvote = models.ForeignKey(get_user_model(), related_name='downvoter', null=True, blank=True, default=None, on_delete=models.CASCADE
-    )
-	class Meta:
-		unique_together=('upvote', 'downvote')
+# class Vote(models.Model):
+# 	upvote = models.ForeignKey(get_user_model(), related_name='upvoter', null=True, blank=True, default=None, on_delete=models.CASCADE
+#     )
+# 	downvote = models.ForeignKey(get_user_model(), related_name='downvoter', null=True, blank=True, default=None, on_delete=models.CASCADE
+#     )
+# 	class Meta:
+# 		unique_together=('upvote', 'downvote')
 
 class Question(models.Model):
 	user = models.ForeignKey(
@@ -17,7 +17,7 @@ class Question(models.Model):
 	description = models.CharField(max_length=800)
 	# tags = models
 	question_date = models.DateTimeField('date published')
-	vote = models.ForeignKey(Vote, on_delete=models.CASCADE, default=None)
+	# vote = models.ForeignKey(Vote, on_delete=models.CASCADE, default=None)
 
 	def __str__(self):
 		return f'{self.question}'
@@ -31,7 +31,7 @@ class Answer(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	answer = models.CharField(max_length=10000)
 	answer_date = models.DateTimeField('date published')
-	vote = models.ForeignKey(Vote, on_delete=models.CASCADE, default=None)
+	# vote = models.ForeignKey(Vote, on_delete=models.CASCADE, default=None)
 
 	def __str__(self):
 		return f'{self.answer}'
@@ -44,6 +44,6 @@ class Comment(models.Model):
 	answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 	comment = models.CharField(max_length= 5000)
 	comment_date = models.DateTimeField('date published')
-	vote = models.ForeignKey(Vote, on_delete=models.CASCADE, default=None)
+	# vote = models.ForeignKey(Vote, on_delete=models.CASCADE, default=None)
 	def __str__(self):
 		return f'{self.comment}'
