98132bc3e06a3a3196c26384a029d711a897ff44
Ujwal <ujwal99arak@gmail.com>
2020-11-03 23:23:07 +0530

    added usersfollowing in admin regiser

--- qaauth/admin.py
+++ qaauth/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, UserFollowing
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(UserFollowing)
